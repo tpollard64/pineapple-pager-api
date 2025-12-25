bfcf403056381abda032e433e32613a92f443874
tpollard64 <pollard010@yahoo.com>
2025-12-24 21:25:02 -0600

    Add GitHub Actions workflow and setup files

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from setuptools import setup, find_packages
+# setup.py
+from setuptools import setup
 
-setup(
-    name="pineapple-pager-api",       
-    version="0.1.0",                         
-    author="tpollard64",
-    description="Python library for interacting with WiFi Pineapple Pager devices",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/tpollard64/pineapple-pager-python-api",
-    packages=find_packages(),
-    install_requires=[
-        "paramiko",                            
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-)
+setup()
